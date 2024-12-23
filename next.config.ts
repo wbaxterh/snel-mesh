4a2b6c8e1c34e34775d15bb085a69eddd6730b56
wbaxterh <wesleybaxterhuber@gmail.com>
2024-12-23 00:32:13 -0500

    skip linter

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  webpack: function (config, options) {
-    config.experiments = {
-      asyncWebAssembly: true,
-      layers: true,
-    };
-    return config;
-  },
+	reactStrictMode: true,
+	eslint: {
+		ignoreDuringBuilds: true, // Disables linting during builds
+	},
+	webpack: function (config, options) {
+		config.experiments = {
+			asyncWebAssembly: true,
+			layers: true,
+		};
+		return config;
+	},
 };
 
 export default nextConfig;
