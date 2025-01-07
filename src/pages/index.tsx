14bfaa6427913e696ee9cdc147a68abbfb411d55
wbaxterh <wesleybaxterhuber@gmail.com>
2025-01-06 20:25:44 -0500

    update discord link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -117,7 +117,6 @@ export default function Home() {
 					</a>
 				</div>
 
-				{/* Display Metrics */}
 				<div className='grid grid-cols-1 md:grid-cols-3 gap-6 mb-8 text-center'>
 					{holderCount !== null && (
 						<div className='p-4 bg-white rounded-lg shadow-md'>
